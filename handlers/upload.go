31d6d32d67c5263d012883f1674c6db51c8de793
Rinat <gitg>
2021-08-06 16:10:16 +0300

    add hashcat integration

--- handlers/upload.go
+++ handlers/upload.go
@@ -1,11 +1,15 @@
 package handlers
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/hashcatAPI/models"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"strings"
 	"syscall"
 )
 
@@ -33,43 +37,38 @@ func (h *UploadHandler) uploadFile(w http.ResponseWriter, r *http.Request) {
 	h.l.Println("File recieved: ", file.Name())
 	h.l.Println("Run hashcat ...")
 	defer os.Remove(file.Name())
-	/*hashcatCMD := exec.Command("hashcat", "-m2500", fileName, "/usr/share/wordlists/fasttrack.txt", "--outfile", "result", "--outfile-format", "1,2", "-l", "10000")
+	hashcatCMD := exec.Command("hashcat", "-m2500", file.Name(), "/usr/share/wordlists/fasttrack.txt", "--outfile", "result", "--outfile-format", "1,2", "-l", "10000")
 	var out bytes.Buffer
 	hashcatCMD.Stdout = &out
 	err = hashcatCMD.Run()
 	if err != nil {
-		log.Println(err)
+		h.l.Println(err)
 	}
 
 	if status := exitStatus(hashcatCMD.ProcessState); status != 0 && status != 1 {
-		fmt.Println("Hashcat error")
+		h.l.Println("Hashcat error")
 		w.WriteHeader(500)
 	} else {
-		file, err := os.Open(fileName + ".crackes")
+		file, err := os.Open("result")
+		defer os.Remove(file.Name())
+		defer file.Close()
 		if err != nil {
 			h.l.Println("No cracked handshakes")
 			w.WriteHeader(200)
 			return
 		}
-		defer file.Close()*/
-
-	//scanner := bufio.NewScanner(file)
-	//addedHash := 0
-	/*for scanner.Scan() {
-		crackedPswd := strings.Split(scanner.Text(), ":")
-		count, _ := h.handshakeRepo.Save(crackedPswd[0], crackedPswd[2], "WPA",  r.Header["Imei"][0], r.Header["Date"][0], crackedPswd[3])
-		addedHash+=count
-	}*/
-	/*if err := scanner.Err(); err != nil {
-		h.l.Println(err)
+		content, _ := ioutil.ReadFile(file.Name())
+		separateContent := strings.Split(string(content), ":")
+		var response struct {
+			Ssid     string `json:"ssid"`
+			Password string `json:"password"`
+			Mac      string `json:"mac"`
+		}
+		response.Password = separateContent[3]
+		response.Ssid = separateContent[2]
+		response.Mac = separateContent[0]
+		json.NewEncoder(w).Encode(response)
 	}
-	h.l.Println("Added ", addedHash, "hashes")
-	err = os.Remove("date:imei.crackes")
-	if err != nil {
-		h.l.Println(err)
-		return
-	}
-	h.l.Println("Temp files removed")*/
 
 }
 
