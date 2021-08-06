ce2e7af251b047049388702f8bd83aa7c6dd8ccf
Rinat <gitg>
2021-08-06 16:28:51 +0300

    add hashcat integration

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -17,7 +18,7 @@ func main() {
 		l.Fatal(err)
 	}
 	statement, _ := db.Prepare("CREATE TABLE IF NOT EXISTS handshakes (id INTEGER PRIMARY KEY, mac TEXT, ssid TEXT, password TEXT)")
-    	statement.Exec()
+	statement.Exec()
 	repo := repositories.NewHandshakeRepository(db)
 	hhs := handlers.NewHandshakes(l, repo)
 	uploadHandler := handlers.NewUpload(l, repo)
@@ -26,8 +27,11 @@ func main() {
 	mux.Handle("/handshakes", hhs).Methods("GET")
 	mux.Handle("/upload", uploadHandler).Methods("POST")
 	s := http.Server{
-		Addr:    ":9000",
-		Handler: mux,
+		Addr:         ":9000",
+		Handler:      mux,
+		IdleTimeout:  100 * time.Second,
+		WriteTimeout: 100 * time.Second,
+		ReadTimeout:  100 * time.Second,
 	}
 	err = s.ListenAndServe()
 	if err != nil {
