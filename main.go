4d7cfae6b995b466f0bef9f6c37a9839031a4f45
Rinat <gitg>
2021-08-08 21:01:58 +0300

    refactoring

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"database/sql"
 	"github.com/gorilla/mux"
 	"github.com/hashcatAPI/app"
-	"log"
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "./data.db")
+	/*db, err := sql.Open("sqlite3", "./data.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	statement, _ := db.Prepare("CREATE TABLE IF NOT EXISTS handshakes (id INTEGER PRIMARY KEY, mac TEXT, ssid TEXT, password TEXT)")
-	statement.Exec()
+	statement.Exec()*/
 
 	mux := mux.NewRouter()
 	app.SetupRouter(mux)
